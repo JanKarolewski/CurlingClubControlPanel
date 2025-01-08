b874b6ea689fd24d970e29e6073ca05bfd3b52ff
Johnny <83114951+JanKarolewski@users.noreply.github.com>
2025-01-08 09:19:44 +0100

    Update settings.py

--- curling_club_website/curling_club_website/settings.py
+++ curling_club_website/curling_club_website/settings.py
@@ -21,12 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5-cich@55%wlm*_a^8#&*m=j)f-%fov@(axad6^@oyi*s$cbub'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
